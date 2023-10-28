9613c9004273b25a503f48205104edeaad40a9c3
Nicolas Schneider <nicolas.schneider@fh-bielefeld.de>
2023-10-28 21:07:48 +0200

    Fixed check_full_line. Add discard stack für Carddeck and player interactions with discard stack.

--- Spielfeld.py
+++ Spielfeld.py
@@ -47,7 +47,7 @@ class GameField:
 
         return field_hidden
 
-    def calculate_sum_player(self, players: list):
+    def calculate_sum_player(self, players: list, card_value_mapping: dict):
         sum_player_dict = {}
         for player in players:
             player_carddeck = player.carddeck_player
@@ -56,7 +56,7 @@ class GameField:
                     sum_player = 0
                     for entry in tup:
                         if entry[2]:
-                            sum_player += player_carddeck.card_value_mapping[str(entry[0])]
+                            sum_player += card_value_mapping[str(entry[0])]
                         else:
                             sum_player += 0
                     sum_player_dict[name] = sum_player
@@ -115,64 +115,107 @@ class GameField:
                     if name == player.name:
                         for entry in array:
                             if entry[1] == position:
-                                if entry[2]:
-                                    entry[0] = player.card_on_hand
-                                    player.card_on_hand = None
-                                    return True
-                                else:
-                                    entry[0] = player.card_on_hand
-                                    player.card_on_hand = None
-                                    entry[2] = True
-                                    return True
+                                card_on_field = entry[0]
+                                entry[0] = player.card_on_hand
+                                player.card_on_hand = None
+                                player.card_on_hand = card_on_field
+                                entry[2] = True
+                                return True
 
     def check_full_line(self):
-        for dic in self.field_hidden:
-            for name, array in dic.items():
+        def count_elements(row):
+            counts = {}
+            for element in row:
+                if element in counts:
+                    counts[element] += 1
+                else:
+                    counts[element] = 1
+            return counts
 
-                values = [entry[0] for entry in array]
-                rows_values = [values[i:i + self.length] for i in range(0, len(values), self.length)]
+        def check_rows_first():
+            for dic in self.field_hidden:
+                for name, array in dic.items():
+                    values = [entry[0] for entry in array]
+                    rows_values = [values[i:i + self.length] for i in range(0, len(values), self.length)]
+                    columns_values = list(zip(*rows_values))
 
-                # Set values of row to 0 if all values are the same
-                for i, row in enumerate(rows_values):
-                    if all(element == row[0] for element in row if element != "-"):
-                        print("in row")
-                        for entry in array:
-                            if entry[1][0] == i and entry[0] != self.star_string:
-                                entry[0] = "-"
-                                entry[2] = True
+                    # Set values of row to 0 if all values are the same
+                    for i, row in enumerate(rows_values):
+                        row_counts = count_elements(row)
+                        if "-" in list(row_counts.keys()):
+                            count_deleted = row_counts["-"]
+                        else:
+                            count_deleted = 0
 
-                columns_values = list(zip(*rows_values))
+                        for key, value in row_counts.items():
+                            if value == 3 and count_deleted == 1 or value == 4 and key != self.star_string:
+                                print("bin drin")
+                                for entry in array:
+                                    if entry[1][0] == i and entry[0] != self.star_string:
+                                        entry[0] = "-"
+                                        entry[2] = True
 
-                # Set column values to 0 if all values are the same
-                for i, column in enumerate(columns_values):
-                    if all(element == column[0] for element in column if element != "-"):
-                        for entry in array:
-                            if entry[1][1] == i and entry[0] != self.star_string:
-                                entry[0] = "-"
-                                entry[2] = True
+            for dic in self.field_hidden:
+                for name, array in dic.items():
+                    values = [entry[0] for entry in array]
+                    rows_values = [values[i:i + self.length] for i in range(0, len(values), self.length)]
+                    columns_values = list(zip(*rows_values))
 
-        for dic in self.field_hidden:
-            for name, array in dic.items():
-                values = [entry[0] for entry in array]
-                rows_values = [values[i:i + self.length] for i in range(0, len(values), self.length)]
-                columns_values = list(zip(*rows_values))
+                    # Set column values to 0 if all values are the same
+                    for i, column in enumerate(columns_values):
+                        column_counts = count_elements(column)
 
-                # Set values of row to 0 if all values are the same
-                for i, row in enumerate(rows_values):
-                    # TODO: Problem with row[0]. It must be indepent of the first element in the row
-                    if all(element == row[0] for element in row if element != "-"):
-                        for entry in array:
-                            if entry[1][0] == i and entry[0] != self.star_string:
-                                entry[0] = "-"
-                                entry[2] = True
+                        for key, value in column_counts.items():
+                            if value == 3 and key != self.star_string:
+                                for entry in array:
+                                    if entry[1][1] == i and entry[0] != self.star_string:
+                                        entry[0] = "-"
+                                        entry[2] = True
 
-                # Set column values to 0 if all values are the same
-                for i, column in enumerate(columns_values):
-                    if all(element == column[0] for element in column if element != "-"):
-                        for entry in array:
-                            if entry[1][1] == i and entry[0] != self.star_string:
-                                entry[0] = "-"
-                                entry[2] = True
+        def check_columns_first():
+            for dic in self.field_hidden:
+                for name, array in dic.items():
+                    values = [entry[0] for entry in array]
+                    rows_values = [values[i:i + self.length] for i in range(0, len(values), self.length)]
+                    columns_values = list(zip(*rows_values))
+
+                    # Set column values to 0 if all values are the same
+                    for i, column in enumerate(columns_values):
+                        column_counts = count_elements(column)
+
+                        for key, value in column_counts.items():
+                            if value == 3 and key != self.star_string:
+                                for entry in array:
+                                    if entry[1][1] == i and entry[0] != self.star_string:
+                                        entry[0] = "-"
+                                        entry[2] = True
+
+            for dic in self.field_hidden:
+                for name, array in dic.items():
+                    values = [entry[0] for entry in array]
+                    rows_values = [values[i:i + self.length] for i in range(0, len(values), self.length)]
+                    columns_values = list(zip(*rows_values))
+
+                    # Set values of row to 0 if all values are the same
+                    for i, row in enumerate(rows_values):
+                        row_counts = count_elements(row)
+                        if "-" in list(row_counts.keys()):
+                            count_deleted = row_counts["-"]
+                        else:
+                            count_deleted = 0
+
+                        for key, value in row_counts.items():
+                            if value == 3 and count_deleted == 1 or value == 4 and key != self.star_string:
+                                for entry in array:
+                                    if entry[1][0] == i and entry[0] != self.star_string:
+                                        entry[0] = "-"
+                                        entry[2] = True
+
+        check_rows_first()
+        # check_columns_first()
+        # check_rows_first()
+        # check_columns_first()
+
     def _set_values(self, player, position: tuple, value):
         for dic in self.field_hidden:
             for name, array in dic.items():
@@ -182,11 +225,13 @@ class GameField:
                             entry[0] = value
                             entry[2] = True
 
+        self.check_full_line()
+
     def __str__(self):
         self.check_full_line()
         sum_player = self.calculate_sum_player(self.player_list)
 
-        string = ""
+        string = f""
         for dic in self.field_hidden:
             for name, array in dic.items():
                 string += f"Name: {name}; Sum: {sum_player[name]}\n"
@@ -198,14 +243,19 @@ class GameField:
                     # if more than game_field.length are added, a new line is started
                     if (array.index(entry) + 1) % self.length == 0:
                         string += "\n"
-                string += "----------------\n"
+                string += "-------------------\n"
         return string
 
 
 class Carddeck:
     def __init__(self):
         self.cards = self.init_carddeck()
+        print("carddeck no discard deck: ", self.cards)
         self.card_value_mapping = self.value_string_mapping()
+        self.discard_stack = [self.cards.pop(0)]
+
+        print("inital Carddeck: ", self.cards)
+        print("initial discard stack: ", self.discard_stack)
 
     def init_carddeck(self):
         cards = [card for card in range(-1, 13) for _ in range(7) if card != 0]
@@ -239,17 +289,38 @@ class Player:
             raise TypeError("Name must be a string")
 
         self.cards = random.sample(carddeck.cards, game_field_dimensions[0] * game_field_dimensions[1])
-        self.carddeck_player = Carddeck()
+        self.carddeck_player = Carddeck
         self.card_on_hand = None
 
     def flip_card(self, position: tuple):
         return position
+
+    def pull_card_from_discard_stack(self, carddeck: Carddeck):
+        if len(carddeck.discard_stack) > 0:
+            if self.card_on_hand is None:
+                # get last card from discard stack
+                card_on_hand = carddeck.discard_stack.pop()
+                self.card_on_hand = card_on_hand
+            else:
+                raise ValueError("Player already has a card on hand! Cannot have more than one card on hand!")
+        else:
+            raise ValueError("Discard stack is empty! Cannot pull card from empty discard stack!")
 
     def pull_card_from_deck(self, carddeck: Carddeck):
         card_on_hand = carddeck.cards[0]
         carddeck.cards.remove(card_on_hand)
         self.card_on_hand = card_on_hand
         return card_on_hand
+
+    def put_card_on_discard_stack(self, carddeck: Carddeck):
+        if self.card_on_hand is not None:
+            carddeck.discard_stack.append(self.card_on_hand)
+            self.card_on_hand = None
+        else:
+            raise ValueError("Player has no card on hand! Cannot put card on discard stack!")
+
+    def __str__(self):
+        return f"Name: {self.name}\nCards: {self.cards}\nCard on hand: {self.card_on_hand}\n"
 
 
 if __name__ == "__main__":
@@ -261,15 +332,11 @@ if __name__ == "__main__":
     G = GameField(4, 3, [S, A])
     star = G.star_string
 
-
-    G._set_values(S, (0, 0), 1)
-    G._set_values(S, (1, 0), 1)
-    G._set_values(S, (2, 0), 1)
-
-    G._set_values(S, (0, 1), 3)
-    G._set_values(S, (0, 2), 3)
-    G._set_values(S, (0, 3), 3)
-
     print(G.field_hidden)
-
     print(G)
+    S.pull_card_from_discard_stack(C)
+    print(S.card_on_hand)
+    G.change_card_with_card_on_hand(S, (0, 0))
+    S.put_card_on_discard_stack(C)
+    print(G)
+    print(S.card_on_hand)
