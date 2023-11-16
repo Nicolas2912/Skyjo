4f7997f00f2ddc1ba7316a915fbaedcb62a45735
Nicolas Schneider <nicolas.schneider@fh-bielefeld.de>
2023-11-16 18:43:36 +0100

    Add RandomAgent

--- agents/simple_reflex_agent.py
+++ agents/simple_reflex_agent.py
@@ -3,6 +3,11 @@ from Game.carddeck import Carddeck
 from Game.gamefield import GameField
 
 
+class RandomAgent:
+
+    def __init__(self):
+        pass
+
 class ReflexAgent:
 
     def __init__(self):
