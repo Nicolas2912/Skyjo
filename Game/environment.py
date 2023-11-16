915029700916cd554edac64299a38e5d269bd4d5
Nicolas Schneider <nicolas.schneider@fh-bielefeld.de>
2023-11-16 18:44:27 +0100

    Optimized state property

--- Game/environment.py
+++ Game/environment.py
@@ -2,6 +2,8 @@ from Game.player import Player
 from Game.carddeck import Carddeck
 from Game.gamefield import GameField
 
+import json
+
 
 class Environment:
 
@@ -10,9 +12,7 @@ class Environment:
         self.players = self.init_players(player_names)
         self.gamefield = self.init_gamefield()
 
-        self.state = []
-        for player in player_names:
-            self.state.append(self.get_state(self.players[player]))
+        self.state = self.init_state()
 
     def init_carddeck(self):
         carddeck = Carddeck()
@@ -32,22 +32,34 @@ class Environment:
 
         return gamefield
 
-    def get_state(self, player: Player, last_action=None):
+    def init_state(self):
         state = {}
 
-        state["player_hand"] = player.card_on_hand
+        for player_name, player in self.players.items():
+            state[player_name] = {}
+            state[player_name]["player_hand"] = player.card_on_hand
 
-        state["field"] = self.gamefield.field_hidden
+            field = self.reformat_field_hidden(player)
+
+            state[player_name]["field"] = field
+
+            state[player_name]["last_action"] = None
 
         state["discard_stack"] = self.carddeck.discard_stack
 
-        state["last_action"] = last_action
-
         # states of game: beginning, running, finished
-
         state["state_of_game"] = self.state_of_game()
 
         return state
+
+    def reformat_field_hidden(self, player: Player):
+        field_hidden = self.gamefield.field_hidden
+        for dic in field_hidden:
+            for name, field in dic.items():
+                if name == player.name:
+                    return field
+
+
 
     def start(self, player_pos_dict: dict):
         # player_pos_dict = {"Player1": [(0,0), (0,1)], "Player2": [(0,2), (0,3)]}
@@ -100,6 +112,13 @@ class Environment:
         # list of allowed actions:
         legal_actions = self.legal_actions()
 
+        # check lega actions
+        last_action = self.state[player.name]["last_action"]
+        if last_action in ["change card", "put discard", None] and action not in ["pull deck", "pull discard"]:
+            raise ValueError("Action is not legal!")
+        elif last_action in ["pull deck", "pull discard"] and action not in ["change card", "put discard"]:
+            raise ValueError("Action is not legal!")
+
         # legal positions ((0,0) - (2,3))
         legal_positions = self.legal_positions()
         legal_positions.append(None)
@@ -114,14 +133,36 @@ class Environment:
 
         if action == "pull deck":
             self.pull_card_deck(player)
+
+            # update self.state
+            self.state[player.name]["player_hand"] = player.card_on_hand
+            self.state[player.name]["field"] = self.reformat_field_hidden(player)
+            self.state[player.name]["last_action"] = action
+
         if action == "pull discard":
-            self.pull_card_deck(player)
+            self.pull_card_discard_stack(player)
+
+            # update self.state
+            self.state[player.name]["player_hand"] = player.card_on_hand
+            self.state[player.name]["field"] = self.reformat_field_hidden(player)
+            self.state[player.name]["last_action"] = action
 
         elif pos is not None and action == "put discard":
             player.put_card_on_discard_stack(self.carddeck)
             self.flip_card_on_field(player, pos)
+
+            # update self.state
+            self.state[player.name]["player_hand"] = player.card_on_hand
+            self.state[player.name]["field"] = self.reformat_field_hidden(player)
+            self.state[player.name]["last_action"] = action
+
         elif pos is not None and action == "change card":
             self.change_card(player, pos)
+
+            # update self.state
+            self.state[player.name]["player_hand"] = player.card_on_hand
+            self.state[player.name]["field"] = self.reformat_field_hidden(player)
+            self.state[player.name]["last_action"] = action
 
     def legal_actions(self):
         legal_actions = ["pull deck", "pull discard", "put discard", "change card"]
@@ -150,14 +191,18 @@ class Environment:
 
 if __name__ == "__main__":
     E = Environment(["Player1", "Player2"])
-    state = E.get_state(E.players["Player1"])
+    state = E.state
 
     E.execute_action(E.players["Player1"], "pull deck")
 
-    state = E.get_state(E.players["Player1"])
+    state = E.state
     print(state)
+    # print(state.keys())
+    # print(state.values())
+    # print(state["Player1"])
 
-    E.execute_action(E.players["Player1"], "change card", (0, 0))
-    print(E.get_state(E.players["Player1"]))
+    E.execute_action(E.players["Player1"], "put discard", (0, 0))
+    # print(E.get_state())
 
+    # state:
     # {player1: {player_hand: "1", field: [['1', (0,0), False], ...]}, player2: {player_hand: "2", field: [['2', (0,0), False], ...]}}
