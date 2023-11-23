11370b2d17b2159f49f51be39b5b4d4f8d568236
Nicolas Schneider <nicolas.schneider@fh-bielefeld.de>
2023-11-23 22:44:00 +0100

    Solve action and position issues in RandomAgent

--- agents/simple_reflex_agent.py
+++ agents/simple_reflex_agent.py
@@ -2,10 +2,11 @@ from Game.player import Player
 from Game.carddeck import Carddeck
 from Game.gamefield import GameField
 from Game.environment import Environment
+# from seed import random_seed
 
-import random
+import random, time
 
-random.seed(42)
+# random.seed(time.process_time())
 
 
 class RandomAgent(Environment):
@@ -25,38 +26,49 @@ class RandomAgent(Environment):
         # self.action_space = self.init_action_space()
 
     def act(self):
+
         if self.state[self.agent_name]["state_of_game"] == "beginning":
             # choose random positions
-            position1 = (random.randint(0, 2), random.randint(0, 3))
-            position2 = (random.randint(0, 2), random.randint(0, 3))
+            all_positions = self.all_positions()
+            position1, position2 = random.sample(all_positions, 2)
 
             action = "flip card"
             self.execute_action(self.agent_player, action, position1)
             self.execute_action(self.agent_player, action, position2)
+            print(f"Agent {self.agent_name} flipped cards at position {position1} and {position2}")
             self.state[self.agent_name]["last_action"] = action
+
+            self.state[self.agent_name]["state_of_game"] = "running"
 
         elif self.state[self.agent_name]["state_of_game"] == "running":
             last_action = self.state[self.agent_name]["last_action"]
-
-            legal_actions = self.legal_actions(last_action)
-            legal_positions = self.legal_positions()
+            legal_actions = self.legal_actions(last_action, self.agent_name)
 
             # choose random action and position
             action = random.choice(legal_actions)
 
+            legal_positions = self.legal_positions(action, self.agent_name)
+
             if action in ["pull deck", "pull discard"]:
                 self.execute_action(self.agent_player, action)
+                print(f"Agent {self.agent_name} executed action {action}")
+                print(f"Agent {self.agent_name} card on hand: {self.agent_player.card_on_hand}")
 
             if action == "put discard":
                 position = random.choice(legal_positions)
                 self.execute_action(self.agent_player, action, position)
+                print(f"Agent {self.agent_name} executed action {action} at position {position}")
 
             elif action == "change card":
                 position = random.choice(legal_positions)
                 self.execute_action(self.agent_player, action, position)
+                print(f"Agent {self.agent_name} executed action {action} and flipped at position {position}")
+
+        elif self.state[self.agent_name]["state_of_game"] == "finished":
+            print(f"Agent {self.agent_name} has finished the game!")
 
         else:
-            raise ValueError("State of game is not valid or game is finished!")
+            raise ValueError("State of game is not valid!")
 
 
 class ReflexAgent:
